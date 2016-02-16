b9a09520351572a29ce0ef8ebe0278560f4cd88c
Samantha.Berry <berrysa@mullenmail.com>
2016-02-16 15:10:41 -0700

    second commit

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
-var ngDemo = angular.module('ngDemo', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-ngDemo.config(function($routeProvider) 
-    {
+myApp.config(function ($routeProvider) 
+    { 
     $routeProvider
-        .when('/', 
-              {
-              templateUrl : 'pages/map.html',
-              controller  : 'mainController'
-			  })
-        .when('/weather', 
-              {
-              templateUrl : 'pages/weather.html',
-              controller  : 'mapController'
-			  });
-	});
+    .when('/',
+        {
+            templateUrl: 'pages/home.html',
+            controller: 'homeController'
+        })
+    .when('/weather',
+        {
+            templateUrl: 'pages/weather.html',
+            controller: 'weatherController'
+        })
+    });
 
-ngDemo.controller('mapController', function($scope) 
+myApp.controller('homeController', ['$scope', function ($scope)
     {
-    $scope.message = 'See what cool things Angular can do!';
-	});
+
+    }]);
+myApp.controller('weatherController', ['$scope', '$http', function ($scope, $http) 
+    {
+        $scope.location = "Denver";
+        $("#dna").click(function()
+            {
+            alert($scope.location);
+            $scope.location = $("#cityIn").val();
+            alert($scope.location);
+            $http.get('http://api.apixu.com/v1/current.json?key=4326f1080d374bd8a6b151825161602&q=' +                     $scope.location.replace(' ', '+'))
+                .success(function(result) {
+                    console.log("You are so smart");
+                })
+                .error(function(data, status) {
+                    console.log("You failed, but you are still smart.");
+                });
+            });
+    }]);
 
 
 $(document).on('click','.navbar-collapse.in',function(e)
