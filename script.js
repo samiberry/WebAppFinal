9ffaac4fd31ee4dd62fbe3400e4827a3a5a1b8fd
Samantha.Berry <berrysa@mullenmail.com>
2016-02-17 08:39:19 -0700

    third commit

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ myApp.controller('weatherController', ['$scope', '$http', function ($scope, $htt
             alert($scope.location);
             $scope.location = $("#cityIn").val();
             alert($scope.location);
-            $http.get('http://api.apixu.com/v1/current.json?key=4326f1080d374bd8a6b151825161602&q=' +                     $scope.location.replace(' ', '+'))
+            $http.get('http://api.apixu.com/v1/current.json?key=4326f1080d374bd8a6b151825161602&q=' + $scope.location.replace(' ', '+'))
                 .success(function(result) {
                     console.log("You are so smart");
                 })
