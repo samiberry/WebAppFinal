1e4f42c154279b38e408c3a8e950dea624b8ffd5
samiberry <berrysa@mullenmail.com>
2016-02-18 05:20:04 -0700

    Update script.js
    Final commit

--- script.js
+++ script.js
@@ -24,12 +24,14 @@ myApp.controller('weatherController', ['$scope', '$http', function ($scope, $htt
         $scope.location = "Denver";
         $("#dna").click(function()
             {
-            alert($scope.location);
             $scope.location = $("#cityIn").val();
-            alert($scope.location);
             $http.get('http://api.apixu.com/v1/current.json?key=4326f1080d374bd8a6b151825161602&q=' + $scope.location.replace(' ', '+'))
                 .success(function(result) {
                     console.log("You are so smart");
+                    $scope.name = result.location.name + ", " + result.location.region;
+                    $scope.country = result.location.country;
+                    $scope.now = result.current.condition.text;
+                    $scope.temp = result.current.temp_f + " degrees Fahrenheit";
                 })
                 .error(function(data, status) {
                     console.log("You failed, but you are still smart.");
